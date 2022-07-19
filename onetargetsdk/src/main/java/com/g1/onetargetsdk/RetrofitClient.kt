776fb32ed991857e0e0f2baad8667de51fe06180
loitp <loitp@galaxy.one>
2022-07-19 16:10:59 +0700

    add log when call api

--- onetargetsdk/src/main/java/com/g1/onetargetsdk/RetrofitClient.kt
+++ onetargetsdk/src/main/java/com/g1/onetargetsdk/RetrofitClient.kt
@@ -1,15 +1,24 @@
 package com.g1.onetargetsdk
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+
 
 object RetrofitClient {
     private var retrofit: Retrofit? = null
 
     fun getClient(baseUrl: String): Retrofit {
         if (retrofit == null) {
+            val interceptor = HttpLoggingInterceptor()
+            interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+            val client: OkHttpClient = OkHttpClient.Builder().addInterceptor(interceptor).build()
+
+
             retrofit = Retrofit.Builder()
                 .baseUrl(baseUrl)
+                .client(client)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
         }
