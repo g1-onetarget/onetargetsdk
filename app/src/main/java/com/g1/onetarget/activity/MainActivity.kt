ad64b86bb9049bd1f7167f9db141a61348043042
loitp <loitp@galaxy.one>
2022-07-19 15:25:51 +0700

    iplm main screen

--- app/src/main/java/com/g1/onetarget/activity/MainActivity.kt
+++ app/src/main/java/com/g1/onetarget/activity/MainActivity.kt
@@ -1,17 +1,43 @@
 package com.g1.onetarget.activity
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.g1.onetarget.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         setupViews()
     }
 
     private fun setupViews() {
+        setupActionBar()
+
+    }
+
+    private fun setupActionBar() {
+        setSupportActionBar(toolbar)
+        supportActionBar?.let { actionBar ->
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setDisplayShowHomeEnabled(true)
+            toolbar.title = getString(R.string.app_name)
+            toolbar.setTitleTextColor(Color.WHITE)
+            toolbar.setNavigationOnClickListener {
+                onBackPressed()
+            }
+        }
+
+        btTestTracking.setOnClickListener {
+            track()
+        }
+    }
+
+    private fun track() {
+
     }
 }
